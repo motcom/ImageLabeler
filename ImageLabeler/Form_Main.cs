29b49c9f6356442535cf9e0b504ada716f84595e
shinichi mochizuki <motti234@qc5.so-net.ne.jp>
2023-11-08 15:23:53 +0900

    プロジェクト ファイルを追加します。

---
+++ ImageLabeler/Form_Main.cs
@@ -0,0 +1,15 @@
+namespace ImageLabeler
+{
+    public partial class Form_Main : Form
+    {
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void TB_Path_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
